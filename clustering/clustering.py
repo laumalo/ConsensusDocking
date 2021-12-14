4c37ac82f9fba33629afff7e6b012b9ec54e3d81
laumalo <laumaro95@gmail.com>
2021-12-14 17:03:36 +0100

    General clustering

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -12,34 +12,45 @@ from collections import Counter
 from biopandas.pdb import PandasPdb
 import linecache
 
+class Clustering(): 
+    def __init__(self, encoding_file, docking_program): 
+        self.encoding_file = encoding_file
+        self.docking_program = docking_program
 
+        # Parsing
+        self.df = self._parse_encoding_file()
 
-def clustering(encoding_file, docking_program):
-    df = pd.read_csv(encoding_file)
-    df.columns=['x1','y1','z1','x2','y2','z2','x3','y3','z3']
-    model = DBSCAN(eps=3*8, metric='euclidean')
-    #model = KMeans(n_clusters=100)
-    model.fit(df.values)
-    clusters = list(model.labels_)
-    top_clusters = sorted([(a,b) for a,b in Counter(model.labels_).items() 
+    def _parse_encoding_file(self):
+            df = pd.read_csv(self.encoding_file)
+            df.columns=['x1','y1','z1','x2','y2','z2','x3','y3','z3']
+            return df
+
+    def _generate_clusters(self, model): 
+        clusters = list(model.labels_)
+        top_clusters = sorted([(a,b) for a,b in Counter(model.labels_).items() 
                    if b>1], key=lambda x: -x[1])
-    #print(top_clusters)
-    labels = model.labels_
-    d = {}
-    
-    file_names = [f'{docking_program}/' + f for f in os.listdir(f'{docking_program}/') if f.endswith(".pdb")] 
-    for k,n in top_clusters:
-        indices = [i for i, v in enumerate(labels) if v==k]
-        files = [file for i,file in enumerate(file_names) if i in indices]
-        d[k] = files
-    print(' - Number of clusters: {}'.format(len(top_clusters)))
-    print(' - Outliers: {}'.format(model.labels_[model.labels_ == -1].size))
-    valid_clusters = [cluster for cluster in top_clusters if not cluster[0] == -1]
-    print(' - Top 10 clusters')
-    for i,cluster in enumerate(valid_clusters[:10]):
-        print('     Cluster {} \t Number of poses: {}'.format(i + 1, cluster[1]))
 
-encoding_file = 'result_ftdock_pdbs.csv'
-docking_program = 'ftdock_pdbs'
-clustering(encoding_file, docking_program)
+        labels = model.labels_
+        d_clusters = {}
+        file_names = [self.docking_program + f for f in os.listdir(self.docking_program) if f.endswith(".pdb")] 
+        for k,n in top_clusters:
+            indices = [i for i, v in enumerate(labels) if v==k]
+            files = [file for i,file in enumerate(file_names) if i in indices]
+        d_clusters[k] = files
+        print(' - Number of clusters: {}'.format(len(top_clusters)))
+        print(' - Outliers: {}'.format(model.labels_[model.labels_ == -1].size))
+        valid_clusters = [cluster for cluster in top_clusters if not cluster[0] == -1]
+        print(' - Top 10 clusters')
+        for i,cluster in enumerate(valid_clusters[:10]):
+            print('     Cluster {} \t Number of poses: {}'.format(i + 1, cluster[1]))
+
+
+    def DBSCAN(self, eps = 3*8):
+        model = DBSCAN(eps=eps, metric='euclidean')
+        model.fit(self.df.values)
+
+        self._generate_clusters(model)
+
+
+
 
