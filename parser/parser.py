714b0703a5326e4685067b2be091e93b40537a67
laumalo <laumaro95@gmail.com>
2021-12-14 17:11:09 +0100

    Add Parsers

--- parser/parser.py
+++ parser/parser.py
@@ -1,3 +1,24 @@
 """
 Parser module
 """
+
+class ParserPIPER():
+	pass 
+
+class ParserZDOCK():
+	pass 
+
+class ParserFTDOCK():
+	pass 
+
+class ParserRosetta():
+	pass 
+
+class ParserLightDock():
+	pass 
+
+class ParserFroDock():
+	pass 
+
+class ParserpPatchDock():
+	pass 
