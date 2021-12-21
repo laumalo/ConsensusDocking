e19f19f766d60b810a72542711c0f0558de75783
annadiarov <annadiarov@gmail.com>
2021-12-21 14:10:26 +0100

    Added Parser

--- parser/parser.py
+++ parser/parser.py
@@ -2,23 +2,48 @@
 Parser module
 """
 
-class ParserPIPER():
-	pass 
 
-class ParserZDOCK():
-	pass 
+class Parser:
+    def __init__(self, program, score_filename, working_dir='.'):
+        """
+        It initializes a specific Parser object for each program.
+        Parameters
+        ----------
+        program : str
+            Name of the program that wants to be parsed.
+        score_filename : str
+            Name of the score file.
+        working_dir : str
+            Path to the folder containing all the folders for each program.
+        """
+        self.program = program.lower()
+        self.score_filename = score_filename
+        self.working_dir = working_dir
 
-class ParserFTDOCK():
-	pass 
+        if self.program == 'rosetta':
+            from parserRosetta import ParserRosetta
+            self.parser = ParserRosetta(self.working_dir, self.score_filename)
+        elif self.program == 'piper':
+            from parserPiper import ParserPiper
+            self.parser = ParserPiper(self.working_dir, self.score_filename)
+        elif self.program == 'frodock':
+            from parserFroDock import ParserFroDock
+            self.parser = ParserFroDock(self.working_dir, self.score_filename)
+        elif self.program == 'patchdock':
+            from parserPatchDock import ParserPatchDock
+            self.parser = ParserPatchDock(self.working_dir, self.score_filename)
+        elif self.program == 'lightdock':
+            from parserLightDock import ParserLightDock
+            self.parser = ParserLightDock(self.working_dir, self.score_filename)
+        elif self.program == 'ftdock':
+            from parserFTDock import ParserFTDock
+            self.parser = ParserFTDock(self.working_dir, self.score_filename)
+        elif self.program == 'zdock':
+            from parserZDock import ParserZDock
+            self.parser = ParserZDock(self.working_dir, self.score_filename)
 
-class ParserRosetta():
-	pass 
-
-class ParserLightDock():
-	pass 
-
-class ParserFroDock():
-	pass 
-
-class ParserpPatchDock():
-	pass 
+    def run(self):
+        """Parses and normalizes scoring files"""
+        self.parser.read()
+        self.parser.norm()
+        self.parser.save()
