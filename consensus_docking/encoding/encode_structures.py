034fd2a709fccb3dacdb47aebab426c7ac8b467e
annadiarov <annadiarov@gmail.com>
2022-02-02 12:22:38 +0100

    Modifing argparser and code to enable the encoding of multiple programs

--- consensus_docking/encoding/encode_structures.py
+++ consensus_docking/encoding/encode_structures.py
@@ -1,5 +1,7 @@
 from encoding import Encoder
 import argparse as ap
+import os
+
 
 def parse_args():
     """
@@ -14,22 +16,32 @@ def parse_args():
         It contains the command-line arguments that are supplied by the user
     """
     parser = ap.ArgumentParser(description="Encoding algorithm.")
-    parser.add_argument("docking_program", type=str,
-                        help="Path to folder containing the PDB files.")
-    parser.add_argument("output", type=str,
-                        help="Path to the output file.")
-    parser.add_argument("-c","--n_proc", type=int,
-                        help='Number of processor.', default = 1)
-    parser.add_argument("--chain", type=str,
-                        help='Chain ID from the ligand protein.', default = 'B')
+    parser.add_argument("program_chain", type=str,
+                        help="Protein-Protein Docking Program underscore chain of the ligand protein (eg. ftdock_B). "
+                             "If you want to analyze different programs, make a coma-separated list without"
+                             " white-spaces.")
+    parser.add_argument("-w", "--w-dir", type=str,
+                        help='Path to folder containing the folders of each program.', default='.')
+    parser.add_argument("-c", "--n-proc", type=int,
+                        help='Number of processor.', default=1)
 
     parsed_args = parser.parse_args()
     return parsed_args
 
+
 def main(args):
-	encoder_tool = Encoder(docking_program = args.docking_program,
-						   chain = args.chain)
-	encoder_tool.run_encoding(output = args.output, n_proc = args.n_proc)
+    output_folder = os.path.join(args.w_dir, 'output', 'encoding')
+    if not os.path.exists(output_folder):
+        print(f'Creating {output_folder}.')
+        os.makedirs(output_folder)
+    input_programs_chain = args.program_chain.split(',')
+    for program_chain in input_programs_chain:
+        program, chain = program_chain.split('_')
+        output_file = os.path.join(output_folder, f'{program}_encoding.csv')
+        encoder_tool = Encoder(docking_program=program, chain=chain.upper(), working_dir=args.w_dir)
+        encoder_tool.run_encoding(output=output_file, n_proc=args.n_proc)
+    # TODO Add option to merge some encodings for the clustering
+
 
 if __name__ == '__main__':
     args = parse_args()
