a5e4a4eb5622b10af56d4f91a075c4dcf1ed7036
annadiarov <annadiarov@gmail.com>
2022-02-10 17:45:30 +0100

    Adding ClusteringDBSCAN, OPTICS and KMeans classes to be importable from outside

--- consensus_docking/clustering/__init__.py
+++ consensus_docking/clustering/__init__.py
@@ -1,2 +1,6 @@
 # __init__.py
 from .clustering import Clustering
+
+from .clusteringDBSCAN import ClusteringDBSCAN
+from .clusteringOPTICS import ClusteringOPTICS
+from .clusteringKMeans import ClusteringKMeans
